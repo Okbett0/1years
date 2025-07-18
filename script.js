ea211dde0475c256b7ac36d07ac41baba98dcea5
Okbett0 <antibaned55@gmail.com>
2025-07-19 00:44:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      const urlParams = new URLSearchParams(window.location.search);
+      const name = urlParams.get("happy 1 year") || "happy 1 year";
+      const elemenUcapan = document.getElementById("ucapan");
+      elemenUcapan.textContent = `Semoga tahun tahun berikutnya penuh kebahagiaan dan kekuatan, ${name}! 🎉`;
+    });
+  </script>
