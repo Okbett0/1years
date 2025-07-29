10d1b18708cd3aef50f95400afdd2241702bd838
Okbett0 <antibaned55@gmail.com>
2025-07-29 08:47:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   const ucapan = document.getElementById("ucapan");
-  ucapan.textContent = "Terima kasih telah bersama selama 1 tahun penuh kenangan dan cinta 💖";
+  ucapan.textContent = "Terima kasih 💖";
 });
